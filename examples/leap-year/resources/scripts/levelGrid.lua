034fb82636e0c9d83736dda168d02818afee41ae
Mikael Lind <elemel@elemel.se>
2019-09-13 09:02:45 +0200

    Add level grid

---
+++ examples/leap-year/resources/scripts/levelGrid.lua
@@ -0,0 +1,6 @@
+return {
+  {"forest", "mountains", "ghostTown", "templeRuins"},
+  {"abandonedMines", "dungeons", "sewers", "catacombs"},
+  {"lakeOfFire", "rockBottom", "hell3", "hell4"},
+  {"heaven1", "tower", "heaven3", "heaven4"},
+}
