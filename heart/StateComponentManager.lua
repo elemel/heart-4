a80e8e633538caccbf0c27c7ec97a72bb84c288c
Mikael Lind <elemel@elemel.se>
2020-05-21 14:52:28 +0200

    Add improvements to state component from plotfarmer

--- heart/StateComponentManager.lua
+++ heart/StateComponentManager.lua
@@ -1,22 +1,46 @@
-local set2 = heart.table.set2
+local class = require("heart.class")
+local heartTable = require("heart.table")
 
-local M = heart.class.newClass()
+local M = class.newClass()
 
 function M:init(game, config)
   self.defaultState = config.defaultState
+
+  if config.validStates then
+    self.defaultState = self.defaultState or config.validStates[1]
+
+    self.validStates = {}
+
+    for _, state in ipairs(config.validStates) do
+      self.validStates[state] = true
+    end
+  end
+
   self.states = {}
   self.stateEntitySets = {}
 end
 
 function M:createComponent(id, config)
   local state = config.state or self.defaultState
+
+  if self.validStates then
+    assert(self.validStates[state], "Invalid state")
+  end
+
   self.states[id] = state
-  set2(self.stateEntitySets, state, id, true)
+
+  if state then
+    heartTable.set2(self.stateEntitySets, state, id, true)
+  end
 end
 
 function M:destroyComponent(id)
   local state = self.states[id]
-  set2(self.stateEntitySets, state, id, nil)
+
+  if state then
+    heartTable.set2(self.stateEntitySets, state, id, nil)
+  end
+
   self.states[id] = nil
 end
 
@@ -24,9 +48,19 @@ function M:setState(id, state)
   local oldState = self.states[id]
 
   if state ~= oldState then
-    set2(self.stateEntitySets, oldState, id, nil)
-    set2(self.stateEntitySets, state, id, true)
+    if self.validStates then
+      assert(self.validStates[state], "Invalid state")
+    end
+
+    if oldState then
+      heartTable.set2(self.stateEntitySets, oldState, id, nil)
+    end
+
     self.states[id] = state
+
+    if state then
+      heartTable.set2(self.stateEntitySets, state, id, true)
+    end
   end
 end
 
