29bc2a7ec1942ce774c523995838eea647416eaa
Mikael Lind <elemel@elemel.se>
2020-09-26 16:07:46 +0200

    Remove obsolete variable

--- examples/spider/spider/FootComponentManager.lua
+++ examples/spider/spider/FootComponentManager.lua
@@ -4,18 +4,15 @@ local M = heart.class.newClass()
 
 function M:init(engine, config)
   self.engine = assert(engine)
-  self.rayIntersections = {}
   self.localJointNormals = {}
 end
 
 function M:createComponent(id, config)
-  self.rayIntersections[id] = {}
   self.localJointNormals[id] = {}
 end
 
 function M:destroyComponent(id)
   self.localJointNormals[id] = nil
-  self.rayIntersections[id] = nil
 end
 
 return M
